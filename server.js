375ea462fa1737cbfab55c4c22f2d4285a5b536f
Basanta-Kc <devbasanta@gmail.com>
2023-03-24 21:48:57 +0545

    add rest api for todos

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const connectionUrl =
@@ -21,7 +25,17 @@ app.get("/", async (req, res) => {
   res.render("todos", { todos: result });
 });
 
+app.get("/todos", async (req, res) => {
+  const result = await Todo.find({
+    name: {
+      $regex: req.query.searchQueryValue,
+    },
+  });
+  res.render("todos", { todos: result });
+});
+
 app.post("/todos", async (req, res) => {
+  console.log(req.body.todo);
   await Todo.create({ name: req.body.todo });
   const result = await Todo.find();
   res.render("todos", { todos: result });
@@ -53,6 +67,53 @@ app.post("/todos/edit/:id", async (req, res) => {
   res.render("todos", { todos: result });
 });
 
+/***
+ *    CRUD API FOR TODOS'S
+ */
+app.get("/api/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json({
+    data: todos,
+  });
+});
+
+app.delete("/api/todos/:id", async (req, res) => {
+  await Todo.deleteOne({ id: req.params.id });
+  res.json({
+    message: "Deleted Successfully.",
+  });
+});
+
+app.patch("/api/todos/:id", async (req, res) => {
+  await Todo.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      name: req.body.todo,
+    }
+  );
+
+  res.json({
+    message: "Updated Successfully.",
+  });
+});
+
+app.post("/api/todos", async (req, res) => {
+  await Todo.create({
+    name: req.body.todo,
+  });
+  res.json({
+    message: "Created Successfully.",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//Accessing infomration form request
+// If its post method (form input) req.body
+// If its get method (form input) req.query
+// If its get or post method and we need to access value from url
+// for eg. /todos/delete/123 => /todos/delete/:id  => req.params.id
